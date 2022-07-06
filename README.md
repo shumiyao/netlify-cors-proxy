665f165fe74cfd7ed05a09dc61726bee4b442856
Shu Miyao <shu.andrade.miyao@gmail.com>
2022-07-06 19:22:44 +0100

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # netlify-cors-proxy
-cors  workaround in accessing to api servers from netlify sites - by using proxy redirect masking.
 
-Something that has been discussed in [Proxy Redirect with CORS issue, site not working?](https://answers.netlify.com/t/proxy-redirect-with-cors-issue-site-not-working/51384).
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,130 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="Click on buttons" />
+    </div>
+  </header>
+
+  <main>
+    <div>Without Proxy: <button @click="withoutProxy">Test</button></div>
+    <div>With Proxy: <button @click="withProxy">Test</button></div>
+    <textarea id="response" v-model="responseText"></textarea>
+  </main>
+</template>
+<script>
+export default {
+  data() {
+    return { responseText: '' };
+  },
+  methods: {
+    async withoutProxy() {
+      this.responseText += 'Without Proxy\n';
+      const a = await this.sendData('https://www.freeforexapi.com/api/live');
+      this.responseText += a + '\n------------\n';
+    },
+    async withProxy() {
+      this.responseText += 'With Proxy\n';
+      const a = await this.sendData('/api-proxy/api/live');
+      this.responseText += a + '\n------------\n';
+    },
+    sendData(url) {
+      return fetch(url)
+        .then(async (response) => {
+          const data = await response.json();
+
+          // check for error response
+          if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+          }
+
+          return data;
+        })
+        .catch((error) => {
+          console.error('There was an error!', error);
+          return error;
+        });
+    },
+  },
+};
+</script>
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+textarea#response {
+  width: 100%;
+  height: 250px;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
